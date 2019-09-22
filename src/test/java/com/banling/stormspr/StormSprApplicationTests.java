ead842c4cdde104aef1a44240722c6ccbdc3526b
Ban <Ban@Ban_Ling>
2019-09-23 01:25:13 +0800

    提交

--- src/test/java/com/banling/stormspr/StormSprApplicationTests.java
+++ src/test/java/com/banling/stormspr/StormSprApplicationTests.java
@@ -1,4 +1,4 @@
-package com.banling.stormsprlocal;
+package com.banling.stormspr;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class StormSprLocalApplicationTests {
+public class StormSprApplicationTests {
 
 	@Test
 	public void contextLoads() {
